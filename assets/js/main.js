e45038a71c246a7ad386cbe3f016dac30d8a665b
Vandriele Barbosa <vandriele.gomes@hotmail.com>
2022-03-14 21:56:28 -0300

    correções
    correção do código + criação da função setResultado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,9 @@
 // Capturar evento de submit do formulário
-const cCalculoIMC = document.querySelector('#calculo-IMC');
+const form = document.querySelector('#calculo-IMC');
 
-cCalculoIMC.addEventListener('subimit', function(e){
-    e.preventDefault();
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
 
     const inpPeso = e.target.querySelector('#peso');
     
@@ -14,25 +15,33 @@ cCalculoIMC.addEventListener('subimit', function(e){
         setResultado('peso invalido', false);
         return;
     }
-
+    
     if(!altura){
         setResultado('Altura invalida', false);
         return;
     }
-
+    
     const imc = pegarImc(peso, altura);
     
 });
-
-function pegarImc(altura, peso){
     
-  const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso',
-  'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+    function pegarImc(altura, peso){
+    
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso',
+    'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+    
+    if (imc >= 39.9) return nivel[5];
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];
+    if (imc < 18.5) return nivel[0];
+}
 
-  if (imc >= 39.9) return nivel[5];
-  if (imc >= 34.9) return nivel[4];
-  if (imc >= 29.9) return nivel[3];
-  if (imc >= 24.9) return nivel[2];
-  if (imc >= 18.5) return nivel[1];
-  if (imc < 18.5) return nivel[0];
+function setResultado(msg){
+    const  resultado = document.querySelector('#resultado')
+    resultado.innerHTML = ' ';
+    const p = document.createElement('p');
+    p.innerHTML = 'Teste';
+    resultado.appendChild(p);
 }
